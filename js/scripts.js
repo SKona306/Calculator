41a4c238f6ea349c1233a3d859bc48a56feacabe
Shaun Kent <shaunkent81@gmail.com>
2021-06-08 19:32:39 -0700

    add conversion function

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,16 @@ function divide(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter a number:"));
-const result = divide(number1, number2);
-alert(result);
+const result1 = divide(number1, number2);
+alert(result1);
 
 
-
+//Function to convert farenheit to celcius//
+function convert(tempF, constant) {
+  return tempF - constant;
+  }
+  const tempF =prompt("Enter a number in Farhenheit:");
+  const constant = 32;
+  const result = convert(tempF, constant);
+  
+  alert(result * 0.556)
